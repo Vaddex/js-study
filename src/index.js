eb54855ac27b1156e4e27f0cbc0dc7c4c14cefd1
Alex Turchynov <oleksandr.turchynov@nixsolutions.com>
2023-03-24 22:12:21 +0200

    Added import

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '/home/vad/Документы/Test111/cra-template-rb/src/myScripts/app.js'
+import './myScripts/task1'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
