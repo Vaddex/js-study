e26facdd5c3e970665c171d634672bea775a3e9c
Vladislav Hos <gosvlad236@gmail.com>
2023-03-24 21:13:58 +0200

    addFirstAppFile

--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '/home/vad/Документы/Test111/cra-template-rb/src/myScripts/app.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
