d4d81b9637571f68e0c6f5a4eee29bc29849e8ae
Vladislav Hos <gosvlad236@gmail.com>
2023-04-23 21:09:27 +0300

    adding a filtering function and then adding the values
    * the function filters the elements of the array and passes the numbers to the new array.
    * output of the filtered data plus method to the console.

--- src/myScripts/app.js
+++ src/myScripts/app.js
@@ -61,24 +61,24 @@
 // console.log(result)
 
 
-let arr = [
-    {id:1},
-    {id:'lol'},
-    {id:3},
-    {id:undefined},
-    {id:5},
-    {id:6},
-    {id:null}
-]
-console.log(arr)
+// let arr = [
+//     {id:1},
+//     {id:'lol'},
+//     {id:3},
+//     {id:undefined},
+//     {id:5},
+//     {id:6},
+//     {id:null}
+// ]
+// console.log(arr)
 
-function filterByID(item){
-    if (Number.isFinite(item.id) && item.id !== 0) {
-        return true
-    }   
-}
-arrByID = arr.filter(filterByID)
-console.log(arrByID)
+// function filterByID(item){
+//     if (Number.isFinite(item.id) && item.id !== 0) {
+//         return true
+//     }   
+// }
+// arrByID = arr.filter(filterByID)
+// console.log(arrByID)
 
 // function arrByID (number){
 //     this.id = this
@@ -89,13 +89,74 @@ console.log(arrByID)
 //         }
 //     }
 // }
-console.log(arrByID)
+// console.log(arrByID)
 
-function arraySum(array){
-let sum = 0;
-for(var i = 0; i < array.length; i++){
-    sum += array[i];
-    }
-console.log(sum)
-}
-arraySum(arrByID)
+// function arraySum(array){
+// let sum = 0;
+// for(var i = 0; i < array.length; i++){
+//     sum += array[i];
+//     }
+// console.log(sum)
+// }
+// arraySum(arrByID)
+
+
+
+// function getSum  (arr) {
+    // let sum = 0
+    // for (let i = 0; i < arr.length; i++) {
+    //     sum += arr [i]
+    // }
+    // function defineProperty () {
+    //         var t=0;for(var i in this)
+    //             if (!isNaN(this[i]))
+    //                 t+=this[i];
+    //         return t;
+    //     }
+    // }
+
+    // export const getSum = (arr) => {
+    //     let sum = 0
+    //     for (const value of arr) {
+    //         if (typeof value === Number) {
+    //           sum += value;
+    //         }
+    //         else if (typeof value !== undefined) {
+    //             return false
+    //         }
+    //         else if (typeof value !== String ) {
+    //             return false
+    //         }
+    //         else if (typeof value !== null) {
+    //             return false
+    //         }
+    //         else if (typeof value !== NaN) {
+    //             return false
+    //         }
+    //       }
+    //     // TODO Make it work with any array
+    // }
+    
+
+
+    // export const getSum1 = (arr) => {
+    //     let sum = 0
+        
+    //     // TODO Make it work with any array
+    // }
+    // function getSum(value) {
+    //     if (typeof value !== 'number') {
+    //         return false;
+    //     }
+    //     return value 
+    // }
+    // let filteredArray = rawArray1.filter(getSum);
+    // console.log(filteredArray)
+    
+    // try {
+    //     console.log(getSum(rawArray1), "should return 15")
+    //     console.log(getSum(rawArray2), "should return 7")
+    //     console.log(getSum(rawArray3), "should return 242")
+    // } catch (e) {
+    //     console.log(e)
+    // }
--- src/myScripts/task1.js
+++ src/myScripts/task1.js
@@ -2,9 +2,16 @@ const rawArray1 = [1, 'lol', 3, undefined, 5, 6, null]
 const rawArray2 = [3,4]
 const rawArray3 = [14, 228, 'NaN']
 
-export const getSum = (arr) => {
-    // TODO Make it work with any array
+export function getSum(value) {
+    if (typeof value !== 'number') {
+        return false
+    }
+    return value 
 }
+let filteredArray = rawArray1.filter(getSum)
+let sum = 0
+filteredArray.map((item) => sum += item)
+console.log(sum) // return 15
 
 try {
     console.log(getSum(rawArray1), "should return 15")
